2452773b57084398461cac6e1357a80e749a53dc
Felipe Netto <flpncc@gmail.com>
2017-08-24 14:12:22 -0300

    Fixing module import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 
 import { ROUTES, COMPONENTS, PARTIAL_COMPONENTS } from './app.routing';
 
