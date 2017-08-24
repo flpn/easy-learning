84ad1b8bb9a51553d91a6ce85862965bbc364567
Felipe Netto <flpncc@gmail.com>
2017-08-24 12:02:02 -0300

    Very basic sign-up's layout

---
+++ src/app/components/sign-up/sign-up.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../../model/user';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
