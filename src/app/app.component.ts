abf58a8026e3f06b5ca352876b2e4602a5469a0b
Felipe Netto <flpncc@gmail.com>
2017-08-25 00:28:15 -0300

    Implementing navbar layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,68 @@
-import { Component } from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+
+import { PAGES } from './utils/constants';
+
+import { User } from './model/user';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  items: FirebaseListObservable<any>;
-  title = 'app works!';
+export class AppComponent  implements OnInit {
 
-  constructor(private af: AngularFireDatabase) {
-    this.items = this.af.list('/items')
+  constructor(private router: Router, private afAuth: AngularFireAuth, private afDatabase: AngularFireDatabase) { }
+
+  ngOnInit() {
+    this.isLogged();
+    this.initTemplate();
+  }
+
+  isLogged() {
+    console.log(this.afAuth.authState.subscribe(user => {
+      if (user) {
+        this.loadUserData();
+      }
+    }));
+  }
+
+  loadUserData() {
+
+  }
+
+  initTemplate() {
+    this.initScreen();
+    this.initTemplateTooltip();
+  }
+
+  initScreen() {
+      $('.button-collapse').sideNav();
+  }
+
+  initTemplateTooltip() {
+      $('.tooltips').tooltip();
+  }
+  
+  closeNav() {
+      $('.button-collapse').sideNav('hide');
+    }
+
+  isNoAuthorizedPage() {
+    return [PAGES.login, PAGES.signUp].some(page => page === this.router.url);
+  }
+
+  goToLogin() {
+    this.router.navigate(['login']);
+  }
+
+  logout() {
+    this.afAuth.auth.signOut()
+      .then(() => this.goToLogin());
   }
 }
---
+++ src/app/utils/constants.ts
@@ -0,0 +1,15 @@
+export const PAGES =  {
+    default: '',
+    login: '/login',
+    signUp: '/signUp',
+    home: '/home'
+}
+
+export const NO_AUTHORIZED_PAGES = {
+    login: '/login',
+    signUp: '/signUp'
+}
+
+export const ENTITIES = {
+    user: '/users'
+}
