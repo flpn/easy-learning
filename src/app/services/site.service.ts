e0f296e47da67987fcc7cbd8d9b0b4830321ddb7
Felipe Netto <flpncc@gmail.com>
2017-08-31 18:32:04 -0300

    Removing inheritance between services

--- src/app/services/site.service.ts
+++ src/app/services/site.service.ts
@@ -13,11 +13,95 @@ import { User } from '../model/user';
 import { Question } from '../model/question';
 
 @Injectable()
-export class SiteService extends BaseService implements OnDestroy {
+export class SiteService implements OnDestroy {
+    subscriptions: any[] = [];
 
-  constructor(private router: Router, private af: AngularFireDatabase, private afAuth: AngularFireAuth) {
-    super(af, afAuth);
-   }
+    constructor(private router: Router, private angularFire: AngularFireDatabase, private angularFireAuth :AngularFireAuth) { }
+
+    ngOnDestroy() {
+        this.subscriptions.forEach(s => s.unsubscribe());
+    }
+
+    insert<T>(entity: string, obj: T):string {
+      return this.angularFire.list(entity).push(obj).key;
+    }
+
+    remove<T>(entity: string, key: string): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+
+            this.angularFire.list(entity).remove(key)
+                .then(() => resolve())
+                .catch(error => reject(error))
+        });
+    }
+
+    retrieve<T>(entity: string, property: string, value: any): Promise<T[]> {
+        return new Promise<T[]>((resolve, reject) => {
+            let subscription = this.angularFire.list(entity, {
+                query: {
+                    orderByChild: property,
+                    equalTo: value
+                }
+                })
+                .subscribe(
+                    result => resolve(result),
+                    error => reject(error)
+                );
+
+            this.subscriptions.push(subscription);
+        })
+    }
+
+    get<T>(entity: string, property: string, value: any): Promise<T> {
+        return new Promise<T>((resolve, reject) => {
+            let subscription = this.angularFire.list(entity, {
+                query: {
+                    orderByChild: property,
+                    equalTo: value
+                }
+                })
+                .subscribe(
+                    result => resolve(result[0]),
+                    error => reject(error)
+                );
+
+            this.subscriptions.push(subscription);
+        })
+    }
+
+    find<T>(entity: string, key: string): Promise<T> {
+        return new Promise<T>((resolve, reject) => {
+            let subscription = this.angularFire.object(`/${entity}/${key}`)
+                .subscribe(entity => resolve(entity))
+
+            this.subscriptions.push(subscription);
+        })
+    }
+
+    update<T>(entity: string, key: string, data: any) {
+        return this.angularFire.list(entity).update(key, data);
+    }
+
+    getObservable<T>(entity: string, property: string, value: any): Observable<T[]> {
+        return this.angularFire.list(entity, {
+            query: {
+                orderByChild: property,
+                equalTo: value
+            }
+        });
+    }
+
+    getCurrentUserEmail(): Promise<string> {
+        return new Promise<string>((resolve, reject) => {
+            this.angularFireAuth.authState.subscribe(user => {
+                if (user != null) {
+                    resolve(user.email);
+                } else {
+                    reject('user is not logged');
+                }
+            })
+        });
+    }  
 
    createQuestion(question: Question) {
      return this.insert<Question>(ENTITIES.question, question)
@@ -28,7 +112,7 @@ export class SiteService extends BaseService implements OnDestroy {
    }
 
    notLogged() {
-    this.afAuth.authState.subscribe(user => {
+    this.angularFireAuth.authState.subscribe(user => {
       if(!user) {
         this.router.navigate([PAGES.login]);
       }
