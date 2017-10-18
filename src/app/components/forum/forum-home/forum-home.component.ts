c7d3a97e7b908799957993808124b21f1a424ee9
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2017-10-18 21:40:36 -0200

    implementada a funcionalidade de filtro de perguntas no forum

--- src/app/components/forum/forum-home/forum-home.component.ts
+++ src/app/components/forum/forum-home/forum-home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import 'rxjs/add/operator/map'
+import { Observable } from 'rxjs/Observable';
+
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
@@ -18,7 +21,8 @@ import { Question } from '../../../model/question';
 })
 export class ForumHomeComponent implements OnInit {
 
-  latestQuestions: FirebaseListObservable<any>;
+  latestQuestions: Observable<any>;
+  searchQuestions: string
 
   constructor(private siteService: SiteService, private router: Router, private auth: AngularFireAuth, private db: AngularFireDatabase) { }
 
@@ -30,4 +34,10 @@ export class ForumHomeComponent implements OnInit {
   goToCreateQuestion() {
     this.router.navigate([PAGES.createQuestion]);
   }
+
+  searchQuestion(){
+      this.latestQuestions = this.db.list(ENTITIES.question)
+                .map(itens => itens.filter(item => item.tags === this.searchQuestions))
+  }
+
 }
